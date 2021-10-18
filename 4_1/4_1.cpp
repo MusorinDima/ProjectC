9ea08aa7e42759e316039eecf37cdc2f1fcc9814
dimam <dimamusorin542@gmail.com>
2021-10-19 00:08:25 +0300

    Задача решена

--- 4_1/4_1.cpp
+++ 4_1/4_1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 int main() {
-
 
     string line;
     ofstream file("C:\\Users\\dimam\\CLionProjects\\ProjectC++\\4_1\\4_1txt");
@@ -17,34 +17,23 @@ int main() {
 
     file.close();
     ifstream fin("C:\\Users\\dimam\\CLionProjects\\ProjectC++\\4_1\\4_1txt");
-    //cout << fin;
     int sum(0);
+    int start = 0;
     string s;
 
 
-        getline(fin,line);
+    getline(fin, line);
+    int finish = line.find(" ");
 
-        s = line.substr(0,line.find(" "));
 
+    for (int i = 0; i < 10; i++) {
 
+        s = line.substr(start, finish - start);
+        start = finish + 1;
+        finish = line.find(" ", start);
         sum += stoi(s);
-        cout << sum << endl;
+    }
+    cout << sum << endl;
+
 
 }
-
-
-
-
-
-
-
-//
-//    int ch = 0;
-//    string line;
-//    for (int i = 1; i < 11; i++) {
-//        file << ch;
-//    }
-//    file.close();
-//    ifstream f("4_1txt");
-//    getline(f,line);
-//    cout << line;
