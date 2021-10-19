18d432b0658922ba2bafe29c53f9dd5a52b8c7cf
KMusorin <k.musorin@gmail.com>
2021-10-19 18:46:40 +0300

    Посмотри как я выставил относительный путь, который будет работать на любом компе. ../ - папка с проектом, дальше твоя созданная папка

--- 3_4/3_4.cpp
+++ 3_4/3_4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     string line;
-    ifstream file("C:\\Users\\dimam\\CLionProjects\\ProjectC++\\3_4\\3_4txt");
+    ifstream file("..\\3_4\\3_4txt");
 
     if(!file.is_open()){
         cout << "file not open";
